bc6eba7c752bfec5e57424cf05d68026b404bef2
jmuehler <86805830+jmuehler@users.noreply.github.com>
2021-10-29 11:39:00 +0200

    Update redirect.js

--- redirect.js
+++ redirect.js
@@ -1,3 +1,3 @@
-var urls = ["https://www.limesurvey.uni-hamburg.de/index.php/admin/questions/sa/view/surveyid/877747/gid/56929/qid/681924", "https://ww3.unipark.de/www/front.php?controller=login&module=survey"];
+var urls = ["https://www.limesurvey.uni-hamburg.de/index.php/113389?lang=de", "https://ww3.unipark.de/uc/hh_fak2_denzmann/277e/"];
 var randNum = Math.floor(Math.random() * urls.length); //generates random number corresponding to a elemnt in urls array
 window.location.replace(urls[randNum]); //redirects to the selected URL
